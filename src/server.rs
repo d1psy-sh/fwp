90aedf318dc6f5e2e8e0ef4f0d38d82dbf11e6c9
nkirschall <niki.nice1203@gmail.com>
2023-03-10 23:38:51 +0100

    feat: post request can now get handled

--- src/server.rs
+++ src/server.rs
@@ -2,14 +2,14 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 
 use bytes::Bytes;
-use http_body_util::Full;
+use http_body_util::{BodyExt, Full};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use tokio::net::TcpListener;
 
 #[tokio::main]
-pub async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+pub async fn start_server() -> Result<(), Box<dyn std::error::Error>> {
     let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
     let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
@@ -24,7 +24,7 @@ pub async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             // Handle the connection from the client using HTTP1 and pass any
             // HTTP requests received on that connection to the `hello` function
             if let Err(err) = http1::Builder::new()
-                .serve_connection(stream, service_fn(hello_world))
+                .serve_connection(stream, service_fn(handler))
                 .await
             {
                 println!("Error serving connection: {:?}", err);
@@ -33,6 +33,23 @@ pub async fn run() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     }
 }
 
-async fn hello_world(_req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("hello hyper"))))
+async fn handler(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    let method = req.method();
+    match *method {
+        hyper::Method::GET => Ok(Response::new(Full::new(Bytes::from("hello hyper")))),
+        hyper::Method::POST => {
+            let incoming = req.into_body();
+            let res_data = incoming.collect().await;
+            match res_data {
+                Ok(data) => {
+                    let body = data.to_bytes();
+                    println!("body: {:?}", body);
+                }
+                Err(e) => println!("error: {:?}", e),
+            }
+            Ok(Response::new(Full::new(Bytes::from("hello post hyper"))))
+        }
+
+        _ => Ok(Response::new(Full::new(Bytes::from("Not Found")))),
+    }
 }
